f53e6bb58423ca9a6c58e15ce90a14a69cc63e8b
Brendan <brendanathoele@gmail.com>
2026-06-15 08:11:19 -0500

    Add home page layout

---
+++ src/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Brendan Thoele</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae pariatur dolore nemo magni incidunt distinctio. Asperiores, cupiditate quod voluptatem quisquam quis at. Et similique repudiandae ipsa odit, iste assumenda neque?</p>
+      <button>PROJECTS</button>
+    </div>
+  )
+}
