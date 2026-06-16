d5547e040770217415c056affbdeae369e86378b
Brendan <brendanathoele@gmail.com>
2026-06-16 15:34:19 -0500

    Position contact block towards middle viewport-height

--- src/ContactCard.jsx
+++ src/ContactCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function ContactCard() {
   return (
-    <div className="h-70 z-5">
-      <div className="fixed flex -right-15 top-10">
+    <div className="h-70 z-5 flex flex-col justify-end">
+      <div className="fixed flex -right-15">
         <ul className="border-2 border-gray-400 p-3 tracking-widest [transform:perspective(500px)_rotateY(-30deg)]">
           <li><h3>Contact:</h3></li>
           <li>brendanathoele@gmail.com</li>
