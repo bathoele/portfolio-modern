ba6218b7b23ded0bcf5ebf6213e8f076814e6840
Brendan <brendanathoele@gmail.com>
2026-06-18 10:09:10 -0500

    Add image hover effects

--- src/NavMenu.jsx
+++ src/NavMenu.jsx
@@ -7,18 +7,18 @@ function NavMenu({ changePage, currentPage }) {
     <nav className="relative -left-65 justify-center z-5 text-lg text-gray-400 font-head">
       <div className="fixed flex h-100 items-center">
         <div className="w-22 [transform:perspective(200px)_rotateY(300deg)] relative left-5">
-          <div className={`${currentPage === "home" ? "border-white" : "border-transparent"} border-1 h-8 w-4 mb-2 ml-auto transition-all duration-300 ease-in-out`}></div>
-          <div className={`${currentPage === "projects" ? "border-white" : "border-transparent"} border-1 h-8 w-4 mb-2 ml-auto transition-all duration-300 ease-in-out`}></div>
-          <div className={`${currentPage === "about" ? "border-white" : "border-transparent"} border-1 h-8 w-4 ml-auto transition-all duration-300 ease-in-out`}></div>
+          <div className={`${currentPage === "home" ? "border-orange-100" : "border-transparent"} border-1 h-8 w-4 mb-2 ml-auto transition-all duration-300 ease-in-out`}></div>
+          <div className={`${currentPage === "projects" ? "border-orange-100" : "border-transparent"} border-1 h-8 w-4 mb-2 ml-auto transition-all duration-300 ease-in-out`}></div>
+          <div className={`${currentPage === "about" ? "border-orange-100" : "border-transparent"} border-1 h-8 w-4 ml-auto transition-all duration-300 ease-in-out`}></div>
         </div>
         <ul className="leading-relaxed flex flex-col w-30 justify-center [transform:perspective(200px)_rotateY(40deg)]">
-          <li className={`${currentPage === "home" ? "text-white" : ""} tracking-widest relative  mb-2 transition-all duration-300 ease-in-out h-8`}>
+          <li className={`${currentPage === "home" ? "text-orange-100" : "hover:text-gray-200"} tracking-widest relative  mb-2 transition-all duration-300 ease-in-out h-8`}>
             <button className="w-full text-start pl-2" onClick={() => changePage("home")}>HOME</button>
           </li>
-          <li className={`${currentPage === "projects" ? "text-white" : ""} tracking-widest relative mb-2 transition-all duration-300 ease-in-out h-8`}>
+          <li className={`${currentPage === "projects" ? "text-orange-100" : "hover:text-gray-200"} tracking-widest relative mb-2 transition-all duration-300 ease-in-out h-8`}>
             <button  className="w-full text-start pl-2" onClick={() => changePage("projects")}>PROJECTS</button>
           </li>
-          <li className={`${currentPage === "about" ? "text-white" : ""} tracking-widest relative  transition-all duration-300 ease-in-out h-8`}>
+          <li className={`${currentPage === "about" ? "text-orange-100" : "hover:text-gray-200"} tracking-widest relative  transition-all duration-300 ease-in-out h-8`}>
             <button className="w-full text-start pl-2" onClick={() => changePage("about")}>ABOUT</button>
           </li>
         </ul>
--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -5,15 +5,15 @@ export default function ProjectCard({ data }) {
 
   return (
     <div className="grid grid-cols-11 gap-5">
-      <div className="col-span-3 border-1 border-orange-100 mt-2 h-fit">
+      <div className="col-span-3 border-1 border-orange-200 mt-2 h-fit">
         <a href="">
           <img src={new URL(`./assets/${data.image}`, import.meta.url).href}
-          alt={data.title} className="w-1/1 relative hover:-top-3 hover:left-2 transition duration-300" />
+          alt={data.title} className="w-1/1 relative transition-all duration-300 ease-in-out hover:-translate-y-3 hover:translate-x-2" />
         </a>
       </div>
       <div className="col-span-8">
         <span>
-          <p className="leading-6.5"><a href="" className="text-xl underline underline-offset-4">{data.title}</a>&ensp;{data.desc}</p>
+          <p className="leading-6.5"><a href="" className="text-xl underline underline-offset-4 hover:decoration-orange-200 hover:text-white">{data.title}</a>&ensp;{data.desc}</p>
         </span>
         {data.tools.length > 0 &&
           <div className="mt-2 flex gap-3 text-sm items-center">
