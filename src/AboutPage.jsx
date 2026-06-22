29f0f4902338ee4daffdc8a70327c2538c3028e3
Brendan <brendanathoele@gmail.com>
2026-06-22 09:38:42 -0500

    Add about content, increase text/box shadow effect, style section borders

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function AboutPage() {
   return (
-    <section id="about" className="flex flex-col gap-5 items-center h-screen w-5/7 mx-auto">
+    <section id="about" className="flex flex-col gap-5 items-center mb-100 w-5/6 mx-auto">
       <h1 className="text-5xl animate-color-pulse font-light font-head tracking-tight">About</h1>
-      <p className="leading-6.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ab accusantium perspiciatis autem ullam corrupti eius a earum commodi. Itaque cumque voluptates eligendi? Facere voluptatum quis molestias nostrum quas nihil.</p>
-      <p className="leading-6.5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eius ipsa iste impedit, soluta quasi vero delectus natus doloremque? Beatae aspernatur recusandae aperiam nam libero ipsa repudiandae, dolor expedita dignissimos?</p>
-      <p className="leading-6.5">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt et, autem error ullam, molestiae assumenda quidem optio ratione nihil magnam aliquam eveniet eum ea, alias exercitationem! Sunt, vitae deleniti! Ab.</p>
+      <p className="leading-6.5">I'm a web developer with an eye for user interface. My interest in graphic design began in high school, when I designed the school spirit club logo and made logos for my friends. While now I pay much more attention towards the structure and syntax of code, I still haven't lost my touch for a frontend design that prioritizes the user.</p>
+      <p className="leading-6.5">In 2024 I discovered a new interest in web development and a renewed interest in learning as a whole. After completing an online web development course that summer, I enrolled in classes and began my pursuit of an Associate's Degree in Web Development.</p>
+      <p className="leading-6.5">In 2025 a friend and I started a brand-building company with an online sauna store as our first project. I took on a web management role and edited our Shopify store to our satisfaction–uploading product information, setting discount rates, and adjusting the Shopify template layout–all with an eye on brand continuity and promoting sales. As we continue to shape the branding and marketing of our various ideas, I continue to learn the core aspects that make a website efficient, beautiful, and unique.</p>
+      <p className="leading-6.5">In my spare time you will probably find me with a guitar in my hands. I have been writing and recording songs for six years now and have yet to lose my imagination for the next beautiful sound.</p>
     </section>
   )
 }
--- src/SectionBorder.jsx
+++ src/SectionBorder.jsx
@@ -4,7 +4,13 @@ export default function SectionBorder() {
   return (
     <div className="flex flex-row items-center justify-center mt-10">
       <div className="w-2 h-2 border-1 border-gray-400 animate-border-left"></div>
-      <div className="border-t-1 animate-border-pulse w-3/5 h-0"></div>
+      <div className="border-t-1 animate-border-pulse w-1/4 h-0"></div>
+      <div className="w-1 h-2 border-1 border-gray-400 animate-border-pulse"></div>
+      <div className="border-t-1 animate-border-pulse w-2 h-0"></div>
+      <div className="w-4 h-4 border-1 border-gray-400 animate-border-pulse"></div>
+      <div className="border-t-1 animate-border-pulse w-2 h-0"></div>
+      <div className="w-1 h-2 border-1 border-gray-400 animate-border-pulse"></div>
+      <div className="border-t-1 animate-border-pulse w-1/4 h-0"></div>
       <div className="w-2 h-2 border-1 border-gray-400 animate-border-right"></div>
     </div>
   )
